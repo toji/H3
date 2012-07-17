5543752fe57170ef77fb0963810ef0e838e507e5
Brandon Jones <txmg64@motorola.com>
2012-07-17 15:47:38 -0700

    Got server working on nodejitsu

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var game = require('./game');
 
 // Node middleware
 var express = require('express');
-var net = require('net');
 var template = require('./template.js');
 
 // File system
@@ -18,8 +17,8 @@ var TCP_PORT = 8181;
 var server = express.createServer();
 
 server.configure(function() {
-    server.use(express.cookieParser());
-    server.use(express.session({ secret: 'baka neko' }));
+    /*server.use(express.cookieParser());
+    server.use(express.session({ secret: 'baka neko' }));*/
     server.use(express.favicon(__dirname + '/public/favicon.ico'));
     server.use(express.static(__dirname + '/public'));
 });
@@ -39,10 +38,10 @@ server.get('/join', function(req, res){
 });
 
 server.get('/game/practice', function(req, res){
-    req.session.game_id = -1;
+    //req.session.game_id = -1;
     
     var context = {
-        game_id: -1,
+        game_id: -1
     };
     
     template.create('public/game.html', context, function(t) {
@@ -69,10 +68,10 @@ server.get('/game/:id?', function(req, res){
         game_id = new_game.id;
     }
     
-    req.session.game_id = game_id;
+    //req.session.game_id = game_id;
 
     var context = {
-        game_id: game_id,
+        game_id: game_id
     };
     
     template.create('public/game.html', context, function(t) {
@@ -80,25 +79,6 @@ server.get('/game/:id?', function(req, res){
         res.end(t);
     });
 });
-
-server.listen(HTTP_PORT);
-console.log('HTTP Server running at on port ' + HTTP_PORT);
-
-// TCP Sever
-var tcp_server = net.createServer(function (socket) {
-    socket.setEncoding("utf8");
-    
-    var new_game = game.Game.find_game();
-    
-    var p = new player.Player();
-    p.bind_tcp_client(socket);
-    p.join_game(new_game);
-    
-    console.log('TCP Player Connected: ' + p.id);
-});
-
-tcp_server.listen(TCP_PORT);
-console.log('TCP Server running at on port ' + TCP_PORT);
 
 // Socket.io
 var io = require('socket.io').listen(server); // Latch onto HTTP server
@@ -116,3 +96,23 @@ io.sockets.on('connection', function(client) {
     var new_game = game.Game.find_game();
     p.join_game(new_game);
 });
+
+server.listen(HTTP_PORT);
+console.log('HTTP Server running at on port ' + HTTP_PORT);
+
+// TCP Sever
+/*var net = require('net');
+var tcp_server = net.createServer(function (socket) {
+    socket.setEncoding("utf8");
+    
+    var new_game = game.Game.find_game();
+    
+    var p = new player.Player();
+    p.bind_tcp_client(socket);
+    p.join_game(new_game);
+    
+    console.log('TCP Player Connected: ' + p.id);
+});
+
+tcp_server.listen(TCP_PORT);
+console.log('TCP Server running at on port ' + TCP_PORT);*/
