581ee331603fbf471200caf65da46596a068109a
Brandon Jones <txmg64@motorola.com>
2012-07-19 22:31:53 -0700

    Fixed touchmove bug on Android

--- public/reels/gameboard.reel/gameboard.js
+++ public/reels/gameboard.reel/gameboard.js
@@ -145,10 +145,12 @@ exports.Gameboard = Montage.create(Component, {
         value: function(event, first) {
             if(this.inputActive) {
                 var target = event.target;
-                var x = event.touches.item(0).pageX - this._element.offsetLeft;
-                var y = event.touches.item(0).pageY - this._element.offsetTop;
+                var x = event.touches[0].pageX - this._element.offsetLeft;
+                var y = event.touches[0].pageY - this._element.offsetTop;
                 
                 this.gameState.addPoint(x, y, first);
+                
+                event.preventDefault();
                 return false;
             }
         }
